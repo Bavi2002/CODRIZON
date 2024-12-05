5ad6bf7b1e67543fe222b74a7a375ca56eb9a905
Bavithran <sivabavithran16@gmail.com>
2024-12-06 02:35:57 +0530

    Added authentication middleware and JWT validation

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,13 @@
 const express = require('express');
+const { protect } = require("../middleware/authMiddleware");
+const { register, login, profile } = require("../controllers/userController");
 
 const router = express.Router();
 
-router.post('/register', (req,res) => {
-    res.send("Register User");
-});
+router.post('/register', register);
 
-router.post('/login', (req,res) => {
-    res.send("Login User");
-});
+router.post('/login', login);
 
-router.get('/profile', (req,res) => {
-    res.send("User Profile");
-});
+router.get('/profile', protect, profile);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
 const connectDb = require("./config/db.js");
-const dotenv =require('dotenv');
-const userRoutes = require('./routes/userRoutes.js');
-const productRoutes = require('./routes/productRoutes.js');
+const userRoutes = require("./routes/userRoutes.js");
+const productRoutes = require("./routes/productRoutes.js");
 
 dotenv.config();
 
@@ -12,18 +14,17 @@ connectDb();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
-app.use('/api/users', userRoutes);
-app.use('/api/products', productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
-app.get('/', (req,res) => {
-    res.send("Api is incoming.....");
+app.get("/", (req, res) => {
+  res.send("Api is incoming.....");
 });
-
 
 //Port Configuration
 const Port = process.env.PORT;
 app.listen(Port, () => {
-    console.log(`Server is running on port ${Port}`);
+  console.log(`Server is running on port ${Port}`);
 });
